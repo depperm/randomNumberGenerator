8f1ae703bbcba75a58962b8493ddc7234497dc52
David Marshall <epper.marshall@gmail.com>
2017-03-25 15:14:20 -0400

    python prngs

---
+++ python/pnrgs.py
@@ -0,0 +1,67 @@
+import time
+
+class RandomGenerators:
+
+  def __init__(self,seed=None,carry=None):
+    #lcg
+    self.lcg_mod=2**31 #10
+    self.lcg_a=1103515245#2
+    self.lcg_c=12345#3
+    #bbs
+    p=492876847 # large prime congruent to 3 ... (p %4==3)
+    q=715225739 # large prime congruent to 3 ... (q %4==3)
+    self.bbs_mod=p*q
+    self.bbs_prev=920419823 # coprime with bbs_mod .... keep it prime for simplicity
+    #mwc
+    self.mwc_b=2**32
+    self.mwc_a=1103515245
+    #seed
+    if seed==None:
+      self.lcg_prev=int(time.time())
+      self.mwc_prev=int(time.time())
+      self.xor_prev=int(time.time())
+    else:
+      self.lcg_prev=seed
+      self.mwc_prev=seed
+      self.xor_prev=seed
+    #carry
+    if carry==None:
+      self.mwc_c=3 # arbitrary carry
+    else:
+      self.mwc_c=carry
+
+  def next_LCG(self):
+    rand_num=(self.lcg_prev*self.lcg_a+self.lcg_c)%self.lcg_mod
+    self.lcg_prev=rand_num
+    return self.lcg_prev/float(self.lcg_mod) # between 0 and 1
+
+  def next_LCG_range(self,mini,maxi):
+    return mini + (int)(self.next_LCG() * ((maxi - mini) + 1))
+
+  def next_BlumBlumShub(self):
+    rand_num=(self.bbs_prev**2) % self.bbs_mod
+    self.bbs_prev=rand_num
+    return rand_num/float(self.bbs_mod) # between 0 and 1
+
+  def next_MWC(self):
+    t=self.mwc_a*self.mwc_prev+self.mwc_c
+    rand_num=t%self.mwc_b
+    self.mwc_prev=rand_num
+    self.mwc_c=int(t/self.mwc_b)
+    return rand_num/float(self.mwc_b)
+
+  def next_xorshift(self):
+    rand_num=self.xor_prev
+    rand_num ^= (rand_num<<21)
+    rand_num ^= (rand_num>>35)
+    rand_num ^= (rand_num<<4)
+    self.xor_prev=rand_num
+    return (rand_num & 0xFFFFFFFF)/float(0xFFFFFFFF) #(& 0xFFFFFFFF trunctates the number), divide by uint32 value for [0,1)
+
+r=RandomGenerators()
+for i in range(10):
+  print('lcg: '+str(r.next_LCG()))
+  print('bbs: '+str(r.next_BlumBlumShub()))
+  print('mwc: '+str(r.next_MWC()))
+  print('xor: '+str(r.next_xorshift()))
+  print('***********')
